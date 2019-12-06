c2f26e226627db7d29936bc4814e8324c5c3aa30
Sam Audino <achilles@Sams-MBP.fios-router.home>
2019-12-06 10:03:26 -0500

    refactoring done

--- anonymizer/anon.py
+++ anonymizer/anon.py
@@ -1,19 +1,19 @@
 # actual anonimizer function
 
-from anon_helper import anonomizer, decrypter, file_checker
+from anon_helper import *
 
 switch_key = ""
 i = 0
 
 while switch_key == "":
-    print("What would you like to do today? Default is anonymize. [anonymize, decrypt, exit]")
+    print("What would you like to do today? Default is anonymize. [encrypt, decrypt, exit]")
 
     temp = input()
 
-    if "anon" in temp.lower():
-        switch_key = True
-    elif "decr" in temp.lower():
+    if "encr" in temp.lower():
         switch_key = False
+    elif "decr" in temp.lower():
+        switch_key = True
     elif "exit" in temp.lower():
         exit()
     else:
@@ -23,65 +23,52 @@ while switch_key == "":
             exit()
         i+=1
 
-if switch_key:
-    print("\n\n\nYou've selected: Anonymize")
-    print("\nPlease enter in the filepath. Hitting return without entering anything will default to the folder that this bash file is currently located in.")
-
-    temp = input()
-
-    user_filepath = "./" if temp == "" else file_checker(temp)
-
-    print("Please enter in the filetype of the exam. Hitting return without entering anything will default to '.ipynb'.")
-
-    temp = input()
-
-    user_filetype = ".ipynb" if temp == "" else temp
-
-    print("Please enter in the exam name. Hitting return without entering anything will default to 'default'.")
-
-    temp = input()
-
-    user_examname = "default" if temp == "" else temp
-
-    print("Please enter in the name of CSV that will store the information to convert the files later. The default is 'conversion.csv'.")
-
-    temp = input()
-
-    user_outputfile = "conversion.csv" if temp == "" else temp
-
-    print("Anonomyzing with settings:","\nFilepath: ", user_filepath,
-          "\nFiletype: ", user_filetype, "\nExam Name: ", user_examname,
-          "\nOutput CSV: ", user_outputfile)
-
-    anonomizer(filepath = user_filepath, filetype = user_filetype,
-               exam_name = user_examname, output_filename = user_outputfile)
-
-    print("\n\n\nAll done! The key to convert the files back is: ", user_outputfile)
-
+if not switch_key:
+    print("\n\n\nYou've selected: Encrypt")
 else:
     print("\n\n\nYou've selected: Decrypt")
-    print("Please enter in the filepath. Hitting return without entering anything will default to the folder that this bash file is currently located in.")
+print("\nPlease enter in the filepath. Hitting return without entering anything will default to the folder that is your PWD.")
+
+temp = input()
+
+user_filepath = "./" if temp == "" else file_checker(temp)
+
+print("\nPlease enter in the filetype of the exam. Hitting return without entering anything will default to '.ipynb'.")
+
+user_filetype = ""
+
+while user_filetype == "":
 
     temp = input()
 
-    user_filepath = "./" if temp == "" else file_checker(temp)
+    if temp == "":
+        user_filetype = ".ipynb"
+    elif temp.lower() in [".r", ".ipynb", ".rmd", ".py"]:
+        user_filetype = temp
+    else:
+        print("\nPlease enter a supported exam file ending.")
 
-    print("Please enter in the filetype of the exam. Hitting return without entering anything will default to '.ipynb'.")
 
-    temp = input()
+print("Please enter in the exam name. Hitting return without entering anything will default to 'default'.")
 
-    user_filetype = ".ipynb" if temp == "" else temp
+temp = input()
 
-    print("Please enter in the name of the CSV containing the encryption data. Hitting return without entering anything will default to 'default'.")
+user_examname = "default" if temp == "" else temp
 
-    temp = input()
+if not switch_key:
+    print("Please enter in the name of CSV that will store the information to convert the files later. The default is 'conversion.csv'.")
+else:
+    print("Please enter the name of the CSV that stores the conversion information. The default is 'conversion.csv'.")
 
-    user_keycsv = "conversion.csv" if temp == "" else temp
+temp = input()
 
-    print("Anonomyzing with settings:","\nFilepath: ", user_filepath,
-          "\nFiletype: ", user_filetype, "\nKey CSV: ", user_keycsv)
+user_outputfile = "conversion.csv" if temp == "" else temp
 
-    decrypter(filepath = user_filepath, filetype = user_filetype,
-              key_csv = user_keycsv)
+print("Anonomyzing with settings:","\nFilepath: ", user_filepath,
+      "\nFiletype: ", user_filetype, "\nExam Name: ", user_examname,
+      "\nOutput CSV: ", user_outputfile)
 
-    print("\n\n\nAll done!")
+encryptor(filepath = user_filepath, filetype = user_filetype,
+           exam_name = user_examname, output_filename = user_outputfile, decrypt = switch_key)
+
+print("\n\n\nAll done! The key to convert the files back is: ", user_outputfile)
--- anonymizer/anon_helper.py
+++ anonymizer/anon_helper.py
@@ -15,8 +15,9 @@ def label_processor(filename):
     Returns:
     student_name: Student Name in the following format: "Student Name"
     '''
+    temp = filename.split("/")[-1]
 
-    temp = re.findall("[A-Za-z]+", filename)
+    temp = re.findall("[A-Za-z]+", temp)
 
     try:
         return temp[0] + " " + temp[1]
@@ -28,15 +29,19 @@ def create_key_df(filepath, filetype, exam_name, output_filename, decrypt):
     Takes a filepath and creates a dataframe containing a conversion key between the
     encoded values and the main value passed. It also saves a CSV version.
     filepath: location of the files you'd like to make into a conversion matrix.
+    filetype: filetype of the exam.
+    exam_name: the exam name to be encoded.
+    output_filename: name of the conversion CSV to be saved as.
+    decrypt: whether to encrypt or decrypt.
 
     Returns:
     encoded_df: a dataframe where the index is the anonymized ID, and the columns are
-    ['student_name', 'orig_file_name', 'encoded_file_name']
+    ['orig_file_name', 'encoded_file_name']
     '''
 
     if decrypt:
         try:
-            return pd.read_csv(filepath+output_filename)
+            return pd.read_csv(filepath+output_filename, index_col = 'id')
         except:
             raise ValueError("The conversion csv could not be found.")
 
@@ -45,18 +50,18 @@ def create_key_df(filepath, filetype, exam_name, output_filename, decrypt):
     exams = [x for x in glob.glob(filepath+"*"+filetype) if ("solution" not in x.lower()) and \
                                                    ("anonomizer" not in x.lower())]
     student_id = random.sample(range(len(exams)), len(exams))
-    encrypted_file = [filepath+str(x)+desc for id in student_id]
+    encrypted_file = [filepath+str(ID)+exam_desc for ID in student_id]
     student_names = [label_processor(x) for x in exams]
 
     temp_dict = {'id' : student_id,
                  'student_name': student_names,
                  'orig_file_name': exams,
-                 'encoded_file_name': encryped_file}
+                 'encoded_file_name': encrypted_file}
 
     temp_df = pd.DataFrame.from_dict(temp_dict)
-    temp_df.set_index('id')
+    temp_df.set_index('id', inplace=True)
 
-    temp_df.to_csv(output_filename)
+    temp_df.to_csv(filepath+output_filename)
 
     return temp_df[['orig_file_name', 'encoded_file_name']]
 
@@ -66,6 +71,7 @@ def renameinator(df, decrypt = False):
     A filepath should not be needed here, as it should be already in the titles from
     the "create_key_df" function.
     df: The dataframe containing the encoding information.
+    decrypt: whether to encrypt or decrypt
 
     Returns:
     nothing, just renames files
@@ -81,7 +87,7 @@ def renameinator(df, decrypt = False):
 def file_checker(filepath):
     '''
     Makes sure the filepath exists before continuing. Fixes typical encoding error
-    where
+    where the final slash is forgotten.
 
     filepath: File path to check.
     '''
@@ -107,11 +113,12 @@ def encryptor(filepath = "./", filetype = ".ipynb", exam_name = "default",
              decrypt the data.
 
     and returns:
-    file_dict: A dictonary of the file name conversions.
+    encoded_df: A dataframe of the file name conversions.
 
     The function will rename the files in that directory, so please be careful using this!
     It will also "save" a CSV of what has been converted. If a CSV with the default name
-    has already been made it will throw an error.
+    has already been made it will throw an error. This choice was made in order to prevent
+    people from encrypting their data twice, by accident.
     '''
 
     filepath = file_checker(filepath)
@@ -120,87 +127,19 @@ def encryptor(filepath = "./", filetype = ".ipynb", exam_name = "default",
         raise ValueError("A file with that name already exists, and cannot be created. Please check that the files \
                           are not already encoded.")
 
-    encoded_df = create_key_df(filepath, decrypt=decrypt)
+    encoded_df = create_key_df(filepath=filepath, filetype=filetype, exam_name=exam_name,
+                               output_filename = output_filename, decrypt=decrypt)
 
     renameinator(df=encoded_df, decrypt=decrypt)
 
+    if decrypt:
+        file_check = glob.glob(filepath+"*"+filetype)
+
+        missing = [file for file in file_check if file not in encoded_df.orig_file_name.values]
+
+        if missing != []:
+            print("The following files have not been processed:")
+            for m in missing:
+                print("\n", m)
+
     return encoded_df
-
-def anonomizer(filepath = "./", filetype = ".ipynb", exam_name = "default", output_filename = "conversion.csv"):
-    '''
-    This function accepts:
-    filepath: a path to where the exams are held, default is PWD
-    filetype: the type of file that should be searched for
-    exam_name: the name of the exam so the anonomized files aren't just 1,2,3... etc.
-    output_filename: the name that they conversion key csv will be saved as.
-
-    and returns:
-    file_dict: A dictonary of the file name conversions.
-
-    The function will rename the files in that directory, so please be careful using this!
-    It will also "save" a CSV of what has been converted. If a CSV with the default name
-    has already been made it will throw an error.
-    '''
-    if filepath[-1] != "/":
-        filepath+= "/"
-
-    if output_filename in os.listdir(path=filepath):
-        print("error goes here (listdir)")
-
-        return ""
-
-    exam_desc = "_"+exam_name+filetype
-    exams = [x for x in glob.glob(filepath+"*"+filetype) if ("solution" not in x.lower()) and \
-                                                   ("anonomizer" not in x.lower())]
-    values = [filepath+str(x)+exam_desc for x in random.sample(range(len(exams)), len(exams))]
-    student_names = [label_processor(x) for x in glob.glob(filepath+"*"+filetype)]
-#    print(exams, values)
-    file_dict = dict(zip(exams, values))
-
-    for x,y in file_dict.items():
-        os.rename(x, y)
-
-    key_csv = pd.DataFrame.from_dict(file_dict, orient="index")
-    key_csv.columns = ['Anonomized']
-    key_csv.index.name = "Original File"
-    key_csv.to_csv(filepath+output_filename)
-
-    return file_dict
-
-def decrypter(filepath = "./", filetype = ".ipynb", key_csv = "conversion.csv"):
-    '''
-    Partner function to anonomizer. Takes anonomized files and decrypts them.
-    filepath: path to the directory
-    filetype: type of file, or list of file types
-    key_csv: the CSV containing the keys in which to convert the data back to CSV.
-
-    If the files are successfully converted back, the key CSV will be deleted.
-    '''
-
-    if filepath[-1] != "/":
-        filepath += "/"
-
-    if key_csv not in os.listdir(path=filepath):
-        print("error goes here (listdir)")
-
-        return ""
-
-
-    key_df = pd.read_csv(filepath+key_csv, index_col = "Original File")
-#     print(key_csv)
-    key_dict = key_df.to_dict(orient = "index")
-
-    for x,y in key_dict.items():
-#         print("old_name: ", y[key_csv.columns[0]], "\nnew_name: ", x)
-        os.rename(y[key_df.columns[0]], x)
-
-    left_overs = [y[key_df.columns[0]] for y in key_dict.values() if y[key_df.columns[0]] in glob.glob(filepath+"*"+filetype)]
-
-    if left_overs != []:
-        print("Unconverted files! Cannot delete the conversion table.")
-        return key_dict
-    else:
-        print("Deleting conversion table.")
-        os.remove(filepath+key_csv)
-
-    return key_dict
